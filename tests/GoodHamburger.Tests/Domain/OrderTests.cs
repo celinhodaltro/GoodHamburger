effd864126161beb956268b3fe0135075997411e
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 17:34:32 -0300

    - Padronizar teste de item duplicado (sanduiches) - Adicionar teste para outras categorias (fritas e drinks)

--- tests/GoodHamburger.Tests/Domain/OrderTests.cs
+++ tests/GoodHamburger.Tests/Domain/OrderTests.cs
@@ -10,16 +10,46 @@ namespace GoodHamburger.Tests.Domain;
 public class OrderTests
 {
     [Fact]
-    public void Should_Not_Allow_Two_Sandwiches()
+    public void Should_Not_Allow_Two_Categories()
     {
-        var burger = new Product("X Burger", ProductCategory.Sandwich, 5m);
-        var egg = new Product("X Egg", ProductCategory.Sandwich, 4.5m);
+        var product1 = new Product("X Burger", ProductCategory.Sandwich, 5m);
+        var product2 = new Product("X Egg", ProductCategory.Sandwich, 4.5m);
 
         var order = new Order();
 
-        order.AddItem(new OrderItem(burger));
+        order.AddItem(new OrderItem(product1));
 
-        var act = () => order.AddItem(new OrderItem(egg));
+        var act = () => order.AddItem(new OrderItem(product2));
+
+        act.Should().Throw<BusinessException>();
+    }
+
+    [Fact]
+    public void Should_Not_Allow_Two_Fries()
+    {
+        var fries1 = new Product("Batata frita", ProductCategory.Fries, 2m);
+        var fries2 = new Product("Batata rústica", ProductCategory.Fries, 3m);
+
+        var order = new Order();
+
+        order.AddItem(new OrderItem(fries1));
+
+        var act = () => order.AddItem(new OrderItem(fries2));
+
+        act.Should().Throw<BusinessException>();
+    }
+
+    [Fact]
+    public void Should_Not_Allow_Two_Drinks()
+    {
+        var drink1 = new Product("Refrigerante", ProductCategory.Drink, 2.5m);
+        var drink2 = new Product("Suco", ProductCategory.Drink, 3m);
+
+        var order = new Order();
+
+        order.AddItem(new OrderItem(drink1));
+
+        var act = () => order.AddItem(new OrderItem(drink2));
 
         act.Should().Throw<BusinessException>();
     }
