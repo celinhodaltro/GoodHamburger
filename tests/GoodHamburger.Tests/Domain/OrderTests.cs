136a571881c50a8ddbed43064aa6db7071d91d01
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 17:27:59 -0300

    Adicionar testes para restrição de items da mesma categoria no pedido

---
+++ tests/GoodHamburger.Tests/Domain/OrderTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using GoodHamburger.Domain.Entities;
+using GoodHamburger.Domain.Enum;
+using GoodHamburger.Domain.Exceptions;
+using GoodHamburger.Domain.Rules;
+using Xunit;
+
+namespace GoodHamburger.Tests.Domain;
+
+public class OrderTests
+{
+    [Fact]
+    public void Should_Not_Allow_Two_Sandwiches()
+    {
+        var burger = new Product("X Burger", ProductCategory.Sandwich, 5m);
+        var egg = new Product("X Egg", ProductCategory.Sandwich, 4.5m);
+
+        var order = new Order();
+
+        order.AddItem(new OrderItem(burger));
+
+        var act = () => order.AddItem(new OrderItem(egg));
+
+        act.Should().Throw<BusinessException>();
+    }
+
+}
