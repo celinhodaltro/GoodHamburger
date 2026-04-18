fb3da6d29d679e4ade2fae0dabfc01af881dd367
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 17:23:47 -0300

    - Deixar Order com função especifica para obrigar a sempre usar o AddItem();

--- src/GoodHamburger.Domain/Entities/Order.cs
+++ src/GoodHamburger.Domain/Entities/Order.cs
@@ -1,9 +1,24 @@
-﻿namespace GoodHamburger.Domain.Entities;
+﻿using GoodHamburger.Domain.Exceptions;
+
+namespace GoodHamburger.Domain.Entities;
 public class Order : Entity
 {
-    public IEnumerable<OrderItem> OrderItems { get; set; } = [];
+    private readonly List<OrderItem> _orderItems = [];
+
+    public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
     public decimal Subtotal { get; set; }
     public decimal Discount { get; set; }
     public decimal PercentDiscount { get; set; }
     public decimal Total { get; set; }
+
+    public OrderItem AddItem(OrderItem orderItem)
+    {
+        if (_orderItems.Any(x => x.Product!.Category == orderItem!.Product.Category))
+            throw new BusinessException("Já existe item dessa categoria.");
+
+        orderItem.OrderId = Id;
+        _orderItems.Add(orderItem);
+
+        return orderItem;
+    }
 }
