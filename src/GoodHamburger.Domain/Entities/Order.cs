2aa1640a80b40120d452459dbada65e5b19748c5
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 12:33:42 -0300

    Adicionar PercentDiscount

--- src/GoodHamburger.Domain/Entities/Order.cs
+++ src/GoodHamburger.Domain/Entities/Order.cs
@@ -4,5 +4,6 @@ public class Order : Entity
     public IEnumerable<OrderItem> OrderItems { get; set; } = [];
     public decimal Subtotal { get; set; }
     public decimal Discount { get; set; }
+    public decimal PercentDiscount { get; set; }
     public decimal Total { get; set; }
 }
