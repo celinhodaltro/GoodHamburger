d9f09b104bb14f687ae1f12fc1b856f64e98ae4a
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 11:44:56 -0300

    Criar DiscountRule (Amarzenar as regras de promoções diretamente no banco de dados)

---
+++ src/GoodHamburger.Domain/Entities/DiscountRule.cs
@@ -0,0 +1,11 @@
+﻿using GoodHamburger.Domain.Enum;
+
+namespace GoodHamburger.Domain.Entities;
+
+public class DiscountRule : Entity
+{
+    public string? Description { get; set; }
+    public decimal Percent { get; set; }
+
+    public IEnumerable<ProductCategory> Categories { get; set; } = [];
+}
