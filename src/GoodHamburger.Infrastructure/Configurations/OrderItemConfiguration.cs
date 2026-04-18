b9f8b18b6f27c336516a78493f5732e027fd35e0
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 12:34:23 -0300

    Mudar configuration para se adequar

--- src/GoodHamburger.Infrastructure/Configurations/OrderItemConfiguration.cs
+++ src/GoodHamburger.Infrastructure/Configurations/OrderItemConfiguration.cs
@@ -19,9 +19,8 @@ public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
         builder.Property(x => x.ProductId)
             .IsRequired();
 
-        builder.HasOne<Product>()
+        builder.HasOne(x => x.Product)
             .WithMany()
-            .HasForeignKey(x => x.ProductId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .HasForeignKey(x => x.ProductId);
     }
 }
