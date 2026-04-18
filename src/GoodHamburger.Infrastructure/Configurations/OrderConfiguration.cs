6df255619a1e5c49d0f0a6e5ae8d2902f593440e
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 12:18:01 -0300

    Adicionar OrderConfiguration

---
+++ src/GoodHamburger.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using GoodHamburger.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GoodHamburger.Infrastructure.Persistence.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Subtotal)
+            .IsRequired();
+
+        builder.Property(x => x.Discount)
+            .IsRequired();
+
+        builder.Property(x => x.Total)
+            .IsRequired();
+
+        builder.HasMany(x => x.OrderItems)
+            .WithOne()
+            .HasForeignKey(x => x.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
