dd6080da2d2036487ddbc68ae6996726c0d23b14
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 12:33:25 -0300

    Criar DiscountRule (Regras fixas e aplicadas no projeto de dominio)

---
+++ src/GoodHamburger.Domain/Rules/DiscountRule.cs
@@ -0,0 +1,35 @@
+﻿using GoodHamburger.Domain.Entities;
+using GoodHamburger.Domain.Enum;
+
+namespace GoodHamburger.Domain.Rules
+{
+    public static class DiscountRule
+    {
+        public static void ApplyDiscountRule(this Order order)
+        {
+            order.Subtotal = order.OrderItems.Sum(x => x.Price);
+
+            var categories = order.OrderItems
+                .Select(x => x.Product.Category)
+                .ToList();
+
+            var hasSandwich = categories.Contains(ProductCategory.Sandwich);
+            var hasFries = categories.Contains(ProductCategory.Fries);
+            var hasDrink = categories.Contains(ProductCategory.Drink);
+
+            order.Discount = 0;
+
+            if (hasSandwich && hasFries && hasDrink)
+                order.PercentDiscount  = 0.20m;
+            else if (hasSandwich && hasDrink)
+                order.PercentDiscount = 0.15m;
+            else if (hasSandwich && hasFries)
+                order.PercentDiscount = 0.10m;
+
+            order.Discount = order.Subtotal * order.Discount;
+            order.Total = order.Subtotal - order.Discount;
+
+        }
+
+    }
+}
