3f0a168f3ecee1512d210d2f7cc7e6b6a9f73f9c
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 12:17:34 -0300

    - Criar contexto - Adicionar bibliotecas no projeto (InMemoryDatabase)

---
+++ src/GoodHamburger.Infrastructure/Contexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GoodHamburger.Domain.Entities;
+using GoodHamburger.Infrastructure.Persistence.Seeds;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodHamburger.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+}
