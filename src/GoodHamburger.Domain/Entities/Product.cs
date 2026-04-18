730ab718b7faf3b2fe7563818df5d595756ec8ef
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 17:24:28 -0300

    Adicionar constructor para Product

--- src/GoodHamburger.Domain/Entities/Product.cs
+++ src/GoodHamburger.Domain/Entities/Product.cs
@@ -3,7 +3,14 @@
 namespace GoodHamburger.Domain.Entities;
 public class Product : Entity
 {
-    public required string Name { get; set; }
+    public Product(string name, ProductCategory category, decimal price)
+    {
+        Name = name;
+        Category = category;
+        Price = price;
+    }
+
+    public string? Name { get; set; }
     public ProductCategory Category { get; set; }
     public decimal Price { get; set; }
 }
