035d249493328bb07bae8f6ce5d0ebca3a7e2655
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 11:21:52 -0300

    - Adicionar propriedades à classe Product (Nome, Categoria, e Valor)

--- src/GoodHamburger.Domain/Entities/Product.cs
+++ src/GoodHamburger.Domain/Entities/Product.cs
@@ -1,4 +1,9 @@
-﻿namespace GoodHamburger.Domain.Entities;
+﻿using GoodHamburger.Domain.Enum;
+
+namespace GoodHamburger.Domain.Entities;
 public class Product : Entity
 {
+    public required string Name { get; set; }
+    public ProductCategory Category { get; set; }
+    public decimal Price { get; set; }
 }
