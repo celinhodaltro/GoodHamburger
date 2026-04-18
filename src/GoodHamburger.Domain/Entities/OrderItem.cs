fa4c18ce23c33d135f5eb5ad7ac51ec721b84e9f
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-18 17:24:06 -0300

    Criar constructor em OrdemItem

--- src/GoodHamburger.Domain/Entities/OrderItem.cs
+++ src/GoodHamburger.Domain/Entities/OrderItem.cs
@@ -2,8 +2,19 @@
 
 public class OrderItem : Entity
 {
+    public OrderItem()
+    {
+    }
+
+    public OrderItem(Product? product)
+    {
+        Product = product;
+        Price = product?.Price ?? 0m;
+        ProductId = product?.Id;
+    }
+
     public decimal Price { get; set; }
-    public Guid OrderId { get; set; }
-    public Guid ProductId { get; set; }
+    public Guid? OrderId { get; set; }
+    public Guid? ProductId { get; set; }
     public Product? Product { get; set; }
 }
