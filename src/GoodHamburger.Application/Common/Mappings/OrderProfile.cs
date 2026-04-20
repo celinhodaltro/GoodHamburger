8888d5f202746cca6b1b76dbca261bb2d707e01d
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-20 18:52:55 -0300

    Criar Common para camada de applicação

---
+++ src/GoodHamburger.Application/Common/Mappings/OrderProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using GoodHamburger.Domain.Entities;
+using GoodHamburger.Shared.DTOs;
+
+namespace GoodHamburger.Application.Products.Mappings;
+
+public class OrderProfile : Profile
+{
+    public OrderProfile()
+    {
+        CreateMap<Order, OrderDto>();
+    }
+}
---
+++ src/GoodHamburger.Application/Common/Mappings/ProductProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using GoodHamburger.Domain.Entities;
+using GoodHamburger.Shared.DTOs;
+
+namespace GoodHamburger.Application.Products.Mappings;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductDto>();
+    }
+}
