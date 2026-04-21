1f779439e948e718a8d7eb9a486abf36b0455485
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-21 14:21:04 -0300

    Deixar padrão

--- src/GoodHamburger.Application/Common/Behaviors/ValidationBehavior.cs
+++ src/GoodHamburger.Application/Common/Behaviors/ValidationBehavior.cs
@@ -13,10 +13,7 @@ public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<
         _validators = validators;
     }
 
-    public async Task<TResponse> Handle(
-        TRequest request,
-        RequestHandlerDelegate<TResponse> next,
-        CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (!_validators.Any())
             return await next();
