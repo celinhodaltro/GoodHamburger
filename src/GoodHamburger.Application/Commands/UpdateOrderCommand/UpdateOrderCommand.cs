70851a67e1bbdf78d4e1ba91547be4fb041a4a00
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-21 14:22:37 -0300

    Criar UpdateOrderCommand

---
+++ src/GoodHamburger.Application/Commands/UpdateOrderCommand/UpdateOrderCommand.cs
@@ -0,0 +1,5 @@
+﻿using GoodHamburger.Shared.DTOs.Requests;
+using MediatR;
+
+namespace GoodHamburger.Application.Commands.UpdateOrderCommand;
+public record UpdateOrderCommand(UpdateOrderRequest request) : IRequest<Guid>;
