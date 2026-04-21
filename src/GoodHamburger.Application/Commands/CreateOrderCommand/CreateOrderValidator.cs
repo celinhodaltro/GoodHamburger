6218ce66c6bd1b734ba6ea0e5aa753ee5adbaa9c
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-21 14:20:41 -0300

    Adicionar validator de CreateOrder

---
+++ src/GoodHamburger.Application/Commands/CreateOrderCommand/CreateOrderValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using GoodHamburger.Domain.Exceptions;
+
+namespace GoodHamburger.Application.Commands.CreateOrderCommand;
+
+public class CreateOrderValidator : AbstractValidator<CreateOrderCommand>
+{
+
+    public CreateOrderValidator()
+    {
+        RuleFor(x => x.request).NotNull()
+                               .WithMessage("A requisição não pode ser nula");
+
+        RuleFor(x => x.request.ProductIds).NotEmpty()
+                                          .WithMessage("A lista de produtos não pode ser vazia");
+    }
+    
+}
