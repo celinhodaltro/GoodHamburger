347e44f11e31d682b60e1cebd3d280e0593ef7d7
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-21 16:38:39 -0300

    Identar GetOrderResponse

--- src/GoodHamburger.Shared/DTOs/Responses/GetOrdersResponse.cs
+++ src/GoodHamburger.Shared/DTOs/Responses/GetOrdersResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace GoodHamburger.Shared.DTOs.Responses;
 
-namespace GoodHamburger.Shared.DTOs.Responses
-{
-    public record GetOrdersResponse(List<OrderDto> Orders);
-}
+public record GetOrdersResponse(List<OrderDto> Orders);
