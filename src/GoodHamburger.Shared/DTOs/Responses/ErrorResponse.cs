958548459d47acc57d6ed2bf95479dd028b09fc6
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-21 21:06:55 -0300

    Criar response para Erro

---
+++ src/GoodHamburger.Shared/DTOs/Responses/ErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace GoodHamburger.Shared.DTOs.Responses;
+
+public class ErrorResponse
+{
+    public string Title { get; set; } = "";
+    public int Status { get; set; }
+    public string Detail { get; set; } = "";
+}
