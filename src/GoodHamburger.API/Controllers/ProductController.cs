efbd34fb92f91635449ba623b3e8bb6460b68edd
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2026-04-21 23:10:07 -0300

    - Trocar MenuController -> ProductController - Criar endpoint (GetProductsByOrderId)

---
+++ src/GoodHamburger.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using GoodHamburger.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodHamburger.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public ProductController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProducts()
+    {
+        var menu = await _mediator.Send(new GetMenuQuery());
+        return Ok(menu);
+    }
+
+    [HttpGet("GetProductsByOrderId/{orderId}")]
+    public async Task<IActionResult> GetProductsByOrderId(Guid orderId)
+    {
+        var products = await _mediator.Send(new GetProductsByOrderIdQuery(orderId));
+        return Ok(products);
+    }
+}
